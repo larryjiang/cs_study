259d5dd91e24cec2811be4715b095622501cd485
larryjiang <ljjy23@yahoo.com>
2018-03-15 23:43:40 +0800

    part of chapter 16

---
+++ system/book_apue/apue_copy/chapter16/ruptime.c
@@ -0,0 +1,42 @@
+#include "../include/apue.h"
+#include <netdb.h>
+#include <errno.h>
+#include <syslog.h>
+#include <sys/socket.h>
+
+
+#define BUFLEN 128
+#define QLEN 10
+
+#ifndef HOST_NAME_MAX
+#define HOST_NAME_MAX 256
+#endif
+
+extern int initserver(int, const struct sockaddr *, socklen_t, int);
+
+
+void serve(int sockfd){
+   int clfd;
+   FILE *fp;
+   char buf[BUFLEN]; 
+   
+   set_cloexec(sockfd);
+   for(;;){
+        if((clfd = accept(sockfd, NULL, NULL)) < 0){
+            syslog(LOG_ERR, "ruptimed: accept error :s", strerror(errno));
+            exit(1);
+        };
+        set_cloexec(clfd);
+        if((fp = popen("/usr/bin/uptime", "r")) == NULL){
+            sprintf(buf, "error: %s\n", strerror(errno));
+            send(clfd, buf, strlen(buf),0);
+        }else{
+            while(fgets(buf, BUFLEN, fp) != NULL){
+                send(clfd, buf, strlen(buf), 0);
+            };
+            pclose(fp);
+        };
+        close(clfd);
+    };
+
+};
