0961983937e608c4c3dba509330f0795b84056a0
larryjiang <ljjy23@yahoo.com>
2018-03-25 20:28:28 +0800

    finish chapter18

---
+++ system/book_apue/apue_copy/chapter17/compileservermainv2usingpoll.sh
@@ -0,0 +1,5 @@
+#! /bin/sh 
+gcc -o client client.c csopenv2.o  ../lib/error.o  cliconnect.o recv_fd.o
+
+gcc -o servermainv2 servermainv2.c poll-loop.o ../lib/error.o ../lib/daemonize.o  handle_request.o serv_listen.o cli_args.o send_err.o buf_args.o send_fd clientmgr.o  serv_accept.o ../lib/errorlog.o
+
