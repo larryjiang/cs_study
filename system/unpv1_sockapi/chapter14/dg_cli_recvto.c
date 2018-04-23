d32445df94987f4a4e19f86e1aa596cb00bf176b
larryjiang <ljjy23@yahoo.com>
2018-04-23 22:56:57 +0800

    add some snippets

---
+++ system/unpv1_sockapi/chapter14/dg_cli_recvto.c
@@ -0,0 +1,28 @@
+#include "../include/lib/unp.h"
+static void sig_alrm(int);
+
+
+void dg_cli(FILE *fp, int sockfd, const SA*pservaddr, socklen_t servlen){
+    int n;
+    char sendline[MAXLINE], recvline[MAXLINE+1];
+    Signal(SIGALRM,sig_alrm);
+    while(Fgets(sendline, MAXLINE, fp) != NULL){
+        Sendto(sockfd,sendline, strlen(sendline),0,pservaddr,servlen);
+        alarm(5);
+        if((n = recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL)) < 0){
+            if(errno == EINTR){
+                fprintf(stderr,"socket timeout\n");
+            }else{
+                alarm(0);
+                recvline[n] = 0;
+                Fputs(recvline,stdout);
+            };
+        };
+
+    };
+};
+
+
+static void sig_alrm(int signo){
+    return;
+}
